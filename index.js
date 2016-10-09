c922afa2ad0e47842242519848eab0e34db70d57
Joao Marcelo Brito <joaomarcelobrito01@gmail.com>
2016-10-09 15:43:59 -0300

    Cleaner examples code

--- index.js
+++ index.js
@@ -22,34 +22,4 @@ class JLang {
 	}
 }
 
-let code = fs.readFileSync('./testCode.jlc').toString();
-let vm = new JLang();
-vm.addFunction('myname', function (name) {
-	console.log(`MY NAME IS: ${name}`);
-	return "Result test";
-});
-let result = vm.execute(code);
-console.log(`RESULT: ${result}`);
-
-/* EXAMPLE CODE */
-
-/*let code = fs.readFileSync('./testCode.jlc').toString();
-console.log('[ CODE ]');
-console.log(code);
-
-let tokens      = Lex(code);
-console.log('[ TOKENS ]');
-console.log(tokens);
-
-let parser = new Parser();
-let tree = parser.parse(tokens);
-console.log('[ PARSE TREE ]');
-console.log(tree);
-
-fs.writeFileSync('./results.json', JSON.stringify(tree));
-
-let vm = new Interpreter();
-vm.run(tree);
-console.log(`Value: ${vm.context.getVar('i')}`);
-*/
-process.exit();
+module.exports = JLang;
